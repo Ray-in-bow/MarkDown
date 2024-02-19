2558dc831d1b2a67f96e4b292198a4de1cba569d
A-nastya-siya <131586927+A-nastya-siya@users.noreply.github.com>
2024-02-19 11:35:52 +0300

    task2

--- C#/Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
+++ C#/Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cb784e0b71c560dffe78fae9ad162922736e4fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58d509bd54b5303d6dfb5d5ca563d17b456afb2a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
