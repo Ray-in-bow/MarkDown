2b156395eb06527139b470d44bbac7e914ddb23a
A-nastya-siya <131586927+A-nastya-siya@users.noreply.github.com>
2024-02-19 12:10:37 +0300

    if

---
+++ C#/Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 9;
+int b = 9;
+
+if (a > b)
+{
+    Console.WriteLine(a);
+}
+else if (a == b)
+{
+    int sum = a + b;
+    Console.WriteLine(sum);
+}
+else
+{
+    Console.WriteLine(b);
+}
