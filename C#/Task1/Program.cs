973519140b98540382870913f4e7165d79a6f90f
A-nastya-siya <131586927+A-nastya-siya@users.noreply.github.com>
2024-02-20 10:11:02 +0300

    all

--- C#/Task1/Program.cs
+++ C#/Task1/Program.cs
@@ -10,3 +10,74 @@ bool b = true;
 Console.WriteLine(d);
 Console.WriteLine(c);
 Console.WriteLine(b);
+
+int i1 = 5;
+int i2 = 6;
+int sum = i1 + i2;
+Console.WriteLine(sum);
+
+long l1 = 6;
+long sum2 = l1 + i1;
+Console.WriteLine(sum2);
+
+double d1 = 5.5;
+double sum3 = d1 + i1;
+Console.WriteLine(sum3);
+
+int product1 = i1 * i2;
+long product2 = l1 * i1;
+double product3 = i1 * d1;
+Console.WriteLine(product1);
+Console.WriteLine(product2);
+Console.WriteLine(product3);
+
+
+int quotient = i1 / i2;
+Console.WriteLine(quotient);
+
+d1 = 6;
+double quotient2 = i1 / d1;
+Console.WriteLine(quotient2);
+
+int a = 9;
+int b1 = 9;
+
+if (a > b1)
+{
+    Console.WriteLine(a);
+}
+else if (a == b1)
+{
+    int summa = a + b1;
+    Console.WriteLine(sum);
+}
+else
+{
+    Console.WriteLine(b);
+}
+
+a = 6;
+int b2 = 7;
+int c1 = 9;
+int d2 = 4;
+int e = 10;
+
+int max = a;
+
+if (b2 > max)
+{
+    max = b2;
+}
+if (c1 > max)
+{
+    max = c1;
+}
+if (d2 > max)
+{
+    max = d2;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
